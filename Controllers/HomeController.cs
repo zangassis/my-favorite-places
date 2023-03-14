d2650972dc955c737e1ec70ed9a1ecffe8847e9f
Assis Zang <assis.zang@gscintegradora.com.br>
2023-03-14 14:08:42 -0300

    feat: Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFavoritePlaces.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace MyFavoritePlaces.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string pathData = "Data/collection.json";
+
+            using var jsonFile = System.IO.File.OpenRead(pathData);
+
+            var items = JsonSerializer.Deserialize<List<PlaceCollectionItem>>(jsonFile);
+
+            return View(items);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/PlaceCollectionItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFavoritePlaces.Models
+{
+    public class PlaceCollectionItem
+    {
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public string? ImageUrl { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
